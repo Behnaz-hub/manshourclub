8140c73e5bd54af93f2b7941a135cb66be427c96
parsabarati <parsabarati83@gmail.com>
2020-01-27 14:00:11 +0330

    noghte

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:carousel_pro/carousel_pro.dart';
 import 'package:manshourclub/cart/cart.dart';
-
+parsa
 void main() {
   runApp(
-      MaterialApp(
-        home: Directionality( // add this
-          textDirection: TextDirection.rtl, // set this property
-          child: MyApp(),
-        ),
+    MaterialApp(
+      home: Directionality( // add this
+        textDirection: TextDirection.rtl, // set this property
+        child: MyApp(),
       ),
+    ),
   );
-  }
+}
 
 class MyApp extends StatefulWidget {
   // This widget is the root of your application.
