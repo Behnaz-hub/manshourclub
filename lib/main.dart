6ddb333338930c794e6a4be58c4b43e3e7e29a21
parsabarati <parsabarati83@gmail.com>
2020-01-27 14:19:35 +0330

    alan

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:carousel_pro/carousel_pro.dart';
 import 'package:manshourclub/cart/cart.dart';
-parsa
+bidibidibidiboooooo
 void main() {
   runApp(
     MaterialApp(
