6ba3cf0b714e9877264b683f662439af0e1429d8
Beynaz <bsafamanesh@yahoo.com>
2020-01-28 10:40:29 +0330

    masn

---
+++ lib/models/senfs.dart
@@ -0,0 +1,47 @@
+import 'dart:convert';
+
+import 'package:manshourclub/utils/webservice.dart';
+
+class Senf {
+  final String aid;
+  final String gid;
+  final String maxLoan;
+  final String numInstallments;
+  final String name;
+  final String details;
+  final String pic;
+  final String icon;
+
+  Senf(
+      {this.aid,
+      this.gid,
+      this.maxLoan,
+      this.numInstallments,
+      this.name,
+      this.details,
+      this.pic,
+      this.icon});
+
+  factory Senf.fromJson(Map<String, dynamic> json) {
+    return Senf(
+        aid: json['aid'],
+        gid: json['gid'],
+        maxLoan: json['maxLoan'],
+        numInstallments: json['numInstallments'],
+        name: json['name'],
+        details: json['details'],
+        pic: json['pic'],
+        icon: json['icon']);
+  }
+
+  static Resource<List<Senf>> get all {
+    return Resource(
+        url: "https://manshourclub.com/API/Loans/AllSenf.php",
+        parse: (response) {
+          final result = json.decode(response.body);
+          Iterable list = result['articles'];
+          print(list);
+          return list.map((model) => Senf.fromJson(model)).toList();
+        });
+  }
+}
